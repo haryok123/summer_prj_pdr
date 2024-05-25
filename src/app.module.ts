c0fe9cdf31977e4815321ed2324ef29d88e9252b
haryok123 <kharyshyn.igor@gmail.com>
2024-05-25 20:51:36 +0300

    create 100 errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,37 @@
 import { Module } from '@nestjs/common';
 import { MainController } from './modules/main/main.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chapter } from './entities/chapter.entity';
+import { Question } from './entities/question.entity';
+import { QuestionTheme } from './entities/question-theme.entity';
+import { Subchapter } from './entities/subchapter.entity';
+import { TheoryItem } from './entities/theory-item.entity';
+import { TheoryItemType } from './entities/theory-item-type.entity';
+import { User } from './entities/user.entity';
+import { MainService } from './modules/main/main.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [
+        Chapter,
+        Question,
+        QuestionTheme,
+        Subchapter,
+        TheoryItem,
+        TheoryItemType,
+        User,
+      ],
     }),
   ],
   controllers: [MainController],
+  providers: [MainService],
+  exports: [MainService],
 })
 export class AppModule {}
--- src/modules/main/main.service.ts
+++ src/modules/main/main.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Chapter } from '../../entities/chapter.entity';
 
 @Injectable()
 export class MainService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Chapter)
+    private readonly chapterRepository: Repository<Chapter>,
+  ) {}
+
+  async findAllChapters(): Promise<Chapter[]> {
+    return this.chapterRepository.find();
+  }
 }
