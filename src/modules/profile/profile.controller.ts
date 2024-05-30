aa60d8875b542470a57db9b953b0ff5ccddbbeca
KateCherednichok <chekavic@gmail.com>
2024-05-30 22:05:45 +0300

    profile

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -1,7 +1,14 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 
-@Controller()
+@Controller('profile')
 export class ProfileController {
   constructor(private profileService: ProfileService) {}
+  @Get()
+  @Render('profile')
+  profile() {
+    return {
+      title: 'Профіль користувача',
+    };
+  }
 }
