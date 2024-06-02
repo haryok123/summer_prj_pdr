c6fd835e4f5628fe4e17051397d522340615928f
KateCherednichok <chekavic@gmail.com>
2024-06-03 01:14:00 +0300

    Update statistics.controller.ts

--- src/modules/statistics/statistics.controller.ts
+++ src/modules/statistics/statistics.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Req, UseGuards } from '@nestjs/common';
 import { StatisticsService } from './statistics.service';
+import { AuthGuard } from '../auth/auth.guard';
+import { Request } from 'express';
 
-@Controller()
+@Controller('statistics')
 export class StatisticsController {
   constructor(private statisticsService: StatisticsService) {}
+  @UseGuards(AuthGuard)
+  @Get('champions-list')
+  @Render('champions-list')
+  renderChampions(@Req() req: Request) {
+    return {
+      title: 'Список Чемпіонів',
+      currentUser: req['user'],
+    };
+  }
 }
